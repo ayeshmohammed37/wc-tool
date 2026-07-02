d01273e13e77313a22fb3a5e04345843383f0f0c
ayeshmohammed <ayeshmohammed37@gmail.com>
2026-07-02 16:55:14 +0300

    use FileInfo to get the size

--- WC-Tool/Program.cs
+++ WC-Tool/Program.cs
@@ -1,13 +1,5 @@
 ﻿
 
-// 1. open the file for reading
-StreamReader myFile = new StreamReader("test.txt");
+FileInfo file = new FileInfo("test.txt");
 
-// 2. Read the first line
-string line = myFile?.ReadLine() ?? "null";
-
-// 3. print the line
-Console.WriteLine(line);
-
-// 4. close the file
-myFile?.Close();
+Console.WriteLine(file.Length);
