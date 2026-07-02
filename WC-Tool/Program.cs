0fc1bb96cb82286166be3eb537b0f0088c98ec67
ayeshmohammed <ayeshmohammed37@gmail.com>
2026-07-02 15:29:14 +0300

    Reading from a file

--- WC-Tool/Program.cs
+++ WC-Tool/Program.cs
@@ -1,1 +1,13 @@
 ﻿
+
+// 1. open the file for reading
+StreamReader myFile = new StreamReader("test.txt");
+
+// 2. Read the first line
+string line = myFile?.ReadLine() ?? "null";
+
+// 3. print the line
+Console.WriteLine(line);
+
+// 4. close the file
+myFile?.Close();
