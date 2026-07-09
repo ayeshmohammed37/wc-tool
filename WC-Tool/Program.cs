cfed5138bf1d3d9a1276fb172ddd846512b61ff2
ayeshmohammed <ayeshmohammed37@gmail.com>
2026-07-09 16:36:45 +0300

    Feature/numberOfBytes Completed

--- WC-Tool/Program.cs
+++ WC-Tool/Program.cs
@@ -7,3 +7,17 @@ var command = prompt.Split(' ')[0];
 var option = prompt.Split(' ')[1];
 var path = prompt.Split(' ')[2];
 
+if (option == "-c")
+{
+    Console.WriteLine(GetFileBytes(path));
+}
+
+
+
+static long GetFileBytes(string path)
+{
+    FileInfo fi = new FileInfo(path);
+    long size = fi.Length;
+
+    return size;
+}
