bd3e329ae50a0a141df18f86f9b6b1830bf1d129
ayeshmohammed <ayeshmohammed37@gmail.com>
2026-07-09 16:28:10 +0300

    extract prompt details

--- WC-Tool/Program.cs
+++ WC-Tool/Program.cs
@@ -2,3 +2,8 @@
 Console.Write(">");
 string prompt = Console.ReadLine();
 
+// extract details of prompt
+var command = prompt.Split(' ')[0];
+var option = prompt.Split(' ')[1];
+var path = prompt.Split(' ')[2];
+
